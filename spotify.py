d971f2d864b1f42dcba470cf6205b6fdf0ef49cb
Benjamin Porter <bporter816@gmail.com>
2020-08-17 01:32:03 -0500

    Support recent songs and top songs endpoints through env var

--- spotify.py
+++ spotify.py
@@ -3,7 +3,15 @@ import json, os, requests, sys
 SPOTIFY_REFRESH_TOKEN = os.getenv('SPOTIFY_REFRESH_TOKEN')
 SPOTIFY_CLIENT_ID     = os.getenv('SPOTIFY_CLIENT_ID')
 SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+SPOTIFY_ENDPOINT      = os.getenv('SPOTIFY_ENDPOINT')
 SPOTIFY_COUNT         = int(os.getenv('SPOTIFY_COUNT'))
+
+if SPOTIFY_ENDPOINT == 'recent':
+    endpoint = '/player/recently-played'
+elif SPOTIFY_ENDPOINT == 'top':
+    endpoint = '/top/tracks'
+else:
+    sys.exit(f'Invalid SPOTIFY_ENDPOINT value "{ SPOTIFY_ENDPOINT }", valid values are "recent" and "top"')
 
 spotify_auth_body = {
     'grant_type': 'refresh_token',
@@ -24,12 +32,30 @@ if 'refresh_token' in res:
 
 spotify_token_type, spotify_access_token = res['token_type'], res['access_token']
 
-q = {'limit': SPOTIFY_COUNT, 'time_range': 'short_term'}
+q = {'limit': 50 if SPOTIFY_ENDPOINT == 'recent' else SPOTIFY_COUNT, 'time_range': 'short_term'}
 h = {'Authorization': f'{ spotify_token_type } { spotify_access_token }'}
-r = requests.get('https://api.spotify.com/v1/me/top/tracks', params=q, headers=h)
+r = requests.get(f'https://api.spotify.com/v1/me{ endpoint }', params=q, headers=h)
 
 if r.status_code != 200:
     sys.exit(f'Error code { r.status_code } getting data from Spotify API')
 
 data = json.loads(r.text)
-print(json.dumps(data['items']))
+
+if SPOTIFY_ENDPOINT == 'recent':
+    songs = {}
+    output = []
+
+    for item in data['items']:
+        song_id = item['track']['id']
+        if song_id in songs:
+            songs[song_id] += 1
+        else:
+            songs[song_id] = 1
+            output.append(item['track'])
+
+    # sort by frequency, then recency
+    output.sort(key=lambda x: songs[x['id']], reverse=True)
+
+    print(json.dumps(output[:SPOTIFY_COUNT]))
+else:
+    print(json.dumps(data['items']))
