387502c0732e318bafd9d4151350776cb2328888
Benjamin Porter <bporter816@gmail.com>
2020-12-29 19:08:17 -0600

    Add playlist option for music

--- spotify.py
+++ spotify.py
@@ -4,16 +4,20 @@ SPOTIFY_REFRESH_TOKEN = os.getenv('SPOTIFY_REFRESH_TOKEN')
 SPOTIFY_CLIENT_ID     = os.getenv('SPOTIFY_CLIENT_ID')
 SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 SPOTIFY_ENDPOINT      = os.getenv('SPOTIFY_ENDPOINT')
+SPOTIFY_PLAYLIST      = os.getenv('SPOTIFY_PLAYLIST')
 SPOTIFY_COUNT         = int(os.getenv('SPOTIFY_COUNT'))
 
 if SPOTIFY_ENDPOINT == 'recent':
-    endpoint = '/player/recently-played'
+    endpoint = 'https://api.spotify.com/v1/me/player/recently-played'
     q = {'limit': 50}
 elif SPOTIFY_ENDPOINT == 'top':
-    endpoint = '/top/tracks'
+    endpoint = 'https://api.spotify.com/v1/me/top/tracks'
     q = {'limit': SPOTIFY_COUNT, 'time_range': 'short_term'}
+elif SPOTIFY_ENDPOINT == 'playlist':
+    endpoint = f'https://api.spotify.com/v1/playlists/{ SPOTIFY_PLAYLIST }/tracks'
+    q = {'limit': SPOTIFY_COUNT, 'fields': 'items(track(name,artists.name,album.images))'}
 else:
-    sys.exit(f'Invalid SPOTIFY_ENDPOINT value "{ SPOTIFY_ENDPOINT }", valid values are "recent" and "top"')
+    sys.exit(f'Invalid SPOTIFY_ENDPOINT value "{ SPOTIFY_ENDPOINT }", valid values are "recent", "top", and "playlist"')
 
 spotify_auth_body = {
     'grant_type': 'refresh_token',
@@ -25,7 +29,7 @@ spotify_auth_body = {
 r = requests.post('https://accounts.spotify.com/api/token', data=spotify_auth_body)
 
 if r.status_code != 200:
-    sys.exit(f'Error code { r.status_code } getting access token from Spotify Accounts Service')
+    sys.exit(f'Error { r.status_code }:{ r.text } getting access token from Spotify Accounts Service')
 
 res = json.loads(r.text)
 
@@ -35,10 +39,10 @@ if 'refresh_token' in res:
 spotify_token_type, spotify_access_token = res['token_type'], res['access_token']
 
 h = {'Authorization': f'{ spotify_token_type } { spotify_access_token }'}
-r = requests.get(f'https://api.spotify.com/v1/me{ endpoint }', params=q, headers=h)
+r = requests.get(endpoint, params=q, headers=h)
 
 if r.status_code != 200:
-    sys.exit(f'Error code { r.status_code } getting data from Spotify API')
+    sys.exit(f'Error { r.status_code }:{ r.text } getting data from Spotify API')
 
 data = json.loads(r.text)
 
@@ -56,7 +60,11 @@ if SPOTIFY_ENDPOINT == 'recent':
 
     # sort by frequency, then recency
     output.sort(key=lambda x: songs[x['id']], reverse=True)
-
     print(json.dumps(output[:SPOTIFY_COUNT]))
-else:
+elif SPOTIFY_ENDPOINT == 'top':
     print(json.dumps(data['items']))
+else:
+    output = []
+    for item in data['items']:
+        output.append(item['track'])
+    print(json.dumps(output))
