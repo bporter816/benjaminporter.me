336ccabb3547b2ea527b8b232f1abeccb1f2d897
Benjamin Porter <bporter816@gmail.com>
2021-07-10 16:58:40 -0500

    Add analytics

--- static/js/profile.js
+++ static/js/profile.js
@@ -3,5 +3,5 @@ var imgsIndex = 0;
 setInterval(() => {
     var elem = document.getElementById("profile");
     imgsIndex = (imgsIndex + 1) % imgs.length;
-    elem.src = `/img/profile/${imgsIndex}.jpg`;
+    elem.src = `/img/profile/${imgs[imgsIndex]}`;
 }, 5000);
