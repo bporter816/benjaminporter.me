eb2c177f3a03004c08079c0d9246c90130ccfc13
Benjamin Porter <bporter816@gmail.com>
2021-11-28 15:53:52 -0600

    Add more emoji (#11)

--- static/js/home.js
+++ static/js/home.js
@@ -1,5 +1,6 @@
 var imgsIndex = 0;
 var taglineIndex = 0;
+var emojiIndex = 0;
 
 setInterval(() => {
     var imgsElem = document.getElementById("profile");
@@ -9,4 +10,8 @@ setInterval(() => {
     var taglineElem = document.getElementById("tagline");
     taglineIndex = (taglineIndex + 1) % taglines.length;
     taglineElem.innerHTML = taglines[taglineIndex];
+
+    var emojiElem = document.getElementById("emoji");
+    emojiIndex = (emojiIndex + 1) % emoji.length;
+    emojiElem.innerHTML = emoji[emojiIndex];
 }, 5000);
