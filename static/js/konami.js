ccfc588cdc83ceb2ec12ff496095e440f1b09a65
Benjamin Porter <bporter816@gmail.com>
2024-07-31 22:59:22 -0400

    Add another Konami action

--- static/js/konami.js
+++ static/js/konami.js
@@ -1,17 +1,27 @@
-var pattern = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"];
+const pattern = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"];
 var index = 0;
 document.addEventListener("keydown", function onEvent(event) {
-    if (event.key === pattern[index]) {
-        index++;
-        if (index === pattern.length) {
-            index = 0;
-            konami();
-        }
-    } else {
-        index = 0;
+  if (event.key === pattern[index]) {
+    index++;
+    if (index === pattern.length) {
+      index = 0;
+      konami();
     }
+  } else {
+    index = 0;
+  }
 });
 
 function konami() {
-    document.getElementById("body").classList.toggle("upside-down");
+  const idx = Math.floor(Math.random() * 2);
+  switch (idx) {
+    case 0:
+      document.getElementById("body").classList.toggle("upside-down");
+      break;
+    case 1:
+      window.open("https://www.youtube.com/watch?v=ia8Q51ouA_s");
+      break;
+    default:
+      alert("Oops");
+  }
 }
