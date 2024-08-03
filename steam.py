f5b15626cbc69af9a90fcb7f5aed3f6135383c7c
Benjamin Porter <bporter816@gmail.com>
2024-08-03 13:16:55 -0400

    Use HTTPS for Steam

--- steam.py
+++ steam.py
@@ -5,7 +5,7 @@ STEAM_USER    = os.getenv('STEAM_USER_ID')
 STEAM_COUNT   = int(os.getenv('STEAM_COUNT'))
 
 q = {'key': STEAM_API_KEY, 'steamid': STEAM_USER, 'count': STEAM_COUNT, 'format': 'json'}
-r = requests.get('http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001', params=q)
+r = requests.get('https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001', params=q)
 
 if r.status_code != 200:
     sys.exit(f'Error code { r.status_code } getting data from Steam API')
