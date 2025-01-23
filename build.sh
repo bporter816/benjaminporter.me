93ecebbf74c17358247dde610a496f144f209027
Benjamin Porter <bporter816@gmail.com>
2025-01-23 01:08:24 -0500

    Tidy up build script

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
+#!/bin/bash
 mkdir -p data
-python3 spotify.py > data/spotify.json
-if [ $? -eq 0 ]; then
+if python3 spotify.py > data/spotify.json; then
     echo "Downloaded Spotify API data to data/spotify.json"
 else
     echo "Failed to download Spotify API data" >&2
     exit 1
 fi
-python3 steam.py > data/steam.json
-if [ $? -eq 0 ]; then
+if python3 steam.py > data/steam.json; then
     echo "Downloaded Steam API data to data/steam.json"
 else
     echo "Failed to download Steam API data" >&2
